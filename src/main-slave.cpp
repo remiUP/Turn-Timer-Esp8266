ec2942ef1b7bd8798774dab723a7f0d04d45953d
remiUP <rdsdf@hotmail.fr>
2024-03-02 02:37:37 +0100

    Initial commit

---
+++ src/main-slave.cpp
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include <ESPNowW.h>
+#include <esp8266wifi.h>
+
+uint8_t receiver_mac[] = {0x8c, 0xaa, 0xb5, 0x69, 0x6b, 0x43};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+	char a[32];
+	int b;
+	float c;
+	String d;
+	bool e;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 2000; // send readings timer
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
+{
+	Serial.print("Last Packet Send Status: ");
+	if (sendStatus == 0)
+	{
+		Serial.println("Delivery success");
+	}
+	else
+	{
+		Serial.println("Delivery fail");
+	}
+}
+
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
+{
+	memcpy(&myData, incomingData, sizeof(myData));
+	Serial.print("Bytes received: ");
+	Serial.println(len);
+	Serial.print("Char: ");
+	Serial.println(myData.a);
+}
+
+void setup()
+{
+	// Init Serial Monitor
+	Serial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
+
+	// Set device as a Wi-Fi Station
+	WiFi.mode(WIFI_STA);
+	// print mac address
+	Serial.print("MAC Address: ");
+	Serial.println(WiFi.macAddress());
+
+	// Init ESP-NOW
+	if (esp_now_init() != 0)
+	{
+		Serial.println("Error initializing ESP-NOW");
+		return;
+	}
+
+	// Once ESPNow is successfully Init, we will register for Send CB to
+	// get the status of Trasnmitted packet
+	esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+	esp_now_register_send_cb(OnDataSent);
+
+	// Register peer
+	Serial.println(esp_now_add_peer(receiver_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0));
+}
+
+void loop()
+{
+	// blink led to indicate data sending
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(500);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(500);
+}
