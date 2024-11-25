09a351e7dd3e924cd50311f49770f208702ab4b5
remiUP <rdsdf@hotmail.fr>
2024-11-25 19:58:18 +0100

    Added constants file

---
+++ lib/Constants/Constants.h
@@ -0,0 +1,27 @@
+
+// WiFi
+#define RETRY_DELAY 500
+#define ACK_TIMEOUT 2000
+
+// EventBroker
+#define EVENTQSIZE 5
+
+// ComStateMachine
+#define MAX_NUMBER_DEVICES 5
+
+// LED
+#define NUM_LEDS 4
+#define LED_DATA_PIN D4
+#define ANIMATION_TIME 3000
+
+// Screen
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+// Buzzer
+#define BUZZER_PIN D5
+
+// Button
+#define BUTTON_PIN D7
