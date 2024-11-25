71d85b54b0ba1c2c65c8ff5b98d3e73f108cc89c
remiUP <rdsdf@hotmail.fr>
2024-11-25 19:58:09 +0100

    Added buzzer

---
+++ lib/Buzzer/Buzzer.cpp
@@ -0,0 +1,15 @@
+#include "Buzzer.h"
+
+Buzzer::Buzzer(uint8_t pin)
+{
+	this->pin = pin;
+	pinMode(pin, OUTPUT);
+}
+
+void Buzzer::beep(unsigned int delay)
+{
+	digitalWrite(pin, LOW);
+	delayMicroseconds(delay);
+	digitalWrite(pin, HIGH);
+	delayMicroseconds(delay);
+}
---
+++ lib/Buzzer/Buzzer.h
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+class Buzzer
+{
+public:
+	Buzzer(uint8_t pin);
+	void beep(unsigned int);
+
+private:
+	uint8_t pin;
+}
